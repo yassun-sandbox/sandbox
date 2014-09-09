9e407c1cb742b88dfb989831c31f1683594eb51a
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-09 20:51:26 +0900

    config.secret_key_base add

---
+++ rails_tutorial_demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+DemoApp::Application.config.secret_key_base =  ENV['SECRET_KEY_BASE'] || 'bc3514e67b8c943dc30f6724cf0d068d6531da06b59d04ae4f96537c2525e41d70b49852f3c3d01558f4dd8d84920556bb4b23b2c855984b933c7acdface241e'
