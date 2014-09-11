63c5e9e9638023a08e34dca36262ee29bf6c7b2c
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-11 19:48:49 +0900

    3-2-2

--- sample_app_rails_4/spec/requests/static_pages_spec.rb
+++ sample_app_rails_4/spec/requests/static_pages_spec.rb
@@ -17,5 +17,13 @@ describe "Static pages" do
     end
   end
 
+  describe "About page" do
+
+    it "should have the content 'About Us'" do 
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+
 end
 
