bfeb9973dc357268531419abcd6b1c1576838578
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-12 18:02:16 +0900

    Finish static pages

--- sample_app_rails_4/spec/requests/static_pages_spec.rb
+++ sample_app_rails_4/spec/requests/static_pages_spec.rb
@@ -8,12 +8,24 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -22,6 +34,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do 
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
