2f335f043720d1891bcbbd1df5012d91ae44fa09
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-15 20:03:13 +0900

    4-5

--- sample_app_rails_4/spec/requests/static_pages_spec.rb
+++ sample_app_rails_4/spec/requests/static_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
