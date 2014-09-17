5a8d6f8e401429fb32e3dfac16e267eb6c42c7cd
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-17 16:02:05 +0900

    5-29

--- sample_app_rails_4/config/routes.rb
+++ sample_app_rails_4/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get "users/new"
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
