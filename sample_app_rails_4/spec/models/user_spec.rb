8b66c1ed0a8ba970654c0e7ce79b353c73943642
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-18 18:20:36 +0900

    6-25

--- sample_app_rails_4/spec/models/user_spec.rb
+++ sample_app_rails_4/spec/models/user_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do 
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -57,6 +62,20 @@ describe User do
 
     it { should_not be_valid }
 
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
   end
 
 end
