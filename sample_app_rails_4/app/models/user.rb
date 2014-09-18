2b9238cc02d60735f381e5a5081458a083514677
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-18 17:17:39 +0900

    6-14

--- sample_app_rails_4/app/models/user.rb
+++ sample_app_rails_4/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
 
