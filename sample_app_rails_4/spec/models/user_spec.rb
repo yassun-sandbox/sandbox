e05ce3edaad9d1d9e8081f2cde349b692588e13c
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-19 17:55:12 +0900

    6-30

--- sample_app_rails_4/spec/models/user_spec.rb
+++ sample_app_rails_4/spec/models/user_spec.rb
@@ -65,6 +65,16 @@ describe User do
 
   end
 
+   describe "email address with mixed case" do
+     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+     it "should be saved as all lower-case" do
+       @user.email = mixed_case_email
+       @user.save
+       expect(@user.reload.email).to eq mixed_case_email.downcase
+     end
+   end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
