d91faab9b7484f8dc3aa1afa85e2b228cf0eb583
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-22 16:50:02 +0900

    7-22

--- sample_app_rails_4/app/controllers/application_controller.rb
+++ sample_app_rails_4/app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
