9c22aa471b8ad152e12e04c0fab79fb23e424074
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-22 16:53:43 +0900

    7-26

--- sample_app_rails_4/app/controllers/application_controller.rb
+++ sample_app_rails_4/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to @user
     else
       render 'new'
     end
