460cba8afae796b5145ecf5498c7e14fbedfedbe
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-25 17:01:13 +0900

    8-14

--- sample_app_rails_4/app/controllers/application_controller.rb
+++ sample_app_rails_4/app/controllers/application_controller.rb
@@ -1,17 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
+  protect_from_forgery with: :exception
+  include SessionsHelper
 end
+
