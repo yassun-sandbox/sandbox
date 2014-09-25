2fc285e2868560ef36ad4ce2536225c88aab46d8
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-25 16:55:42 +0900

    8-12

--- sample_app_rails_4/app/controllers/sessions_controller.rb
+++ sample_app_rails_4/app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーをサインインさせ、ユーザーページ (show) にリダイレクトする。
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
