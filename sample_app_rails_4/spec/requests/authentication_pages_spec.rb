17dbf6c497ececdc360c2e2290526e841aeb3216
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-25 16:24:42 +0900

    8-5

--- sample_app_rails_4/spec/requests/authentication_pages_spec.rb
+++ sample_app_rails_4/spec/requests/authentication_pages_spec.rb
@@ -10,5 +10,20 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      # ページ遷移してない
+      it { should have_title('Sign in') }
+
+      # エラーメッセージが出ているか
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+    end
+  end
 end
 
