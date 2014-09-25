59cbe018b020294e8784b4866e3e04c862aca7e7
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-25 17:22:40 +0900

    8-19

--- sample_app_rails_4/app/helpers/sessions_helper.rb
+++ sample_app_rails_4/app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  def sign_in(user)
+
+    # トークンを新規作成する。
+    remember_token = User.new_remember_token
+
+    # 暗号化されていないトークンをブラウザのcookiesに保存する
+    cookies.permanent[:remember_token] = remember_token
+
+    # 暗号化したトークンをデータベースに保存する
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+
+    # 与えられたユーザーを現在のユーザーに設定する
+    self.current_user = user
+
+  end
+
 end
