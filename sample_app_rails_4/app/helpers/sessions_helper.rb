85e52507e5380fbfb6ca8d683169adc3c970950b
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-25 17:35:47 +0900

    8-22

--- sample_app_rails_4/app/helpers/sessions_helper.rb
+++ sample_app_rails_4/app/helpers/sessions_helper.rb
@@ -15,4 +15,13 @@ module SessionsHelper
 
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.encrypt(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
 end
