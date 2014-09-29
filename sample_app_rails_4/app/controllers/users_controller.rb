2206cc71470d46e5208d40f8096f64df1322f8eb
Taga Yasunori <yasukun.taga@gmail.com>
2014-09-29 17:24:19 +0900

    9-21

--- sample_app_rails_4/app/controllers/users_controller.rb
+++ sample_app_rails_4/app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
   end
