d6eda17cba03a7ebc440f8af39be5cc54f67fedb
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-01 19:10:58 +0900

    9-44

--- sample_app_rails_4/app/controllers/users_controller.rb
+++ sample_app_rails_4/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :signed_in_user, only: [:edit, :update, :index, :destory]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -36,6 +36,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
 
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   private
