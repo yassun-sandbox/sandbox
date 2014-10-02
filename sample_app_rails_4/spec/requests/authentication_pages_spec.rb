c96cf00b8c3374d7133b494e267b7427919a558b
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 18:36:57 +0900

    10-23

--- sample_app_rails_4/spec/requests/authentication_pages_spec.rb
+++ sample_app_rails_4/spec/requests/authentication_pages_spec.rb
@@ -81,6 +81,20 @@ describe "Authentication" do
             end
           end
 
+          # ログインした際にMicropostにアクセスした場合
+          describe "in the Microposts controller" do
+
+            describe "submitting to the create action" do
+              before { post microposts_path }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before { delete micropost_path(FactoryGirl.create(:micropost)) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+          end
+
           # ログインしていない場合
           describe "in the Users controller" do
 
