eb55e8663aba181e07edc53ff175281476a2afee
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 20:33:56 +0900

    10-46

--- sample_app_rails_4/app/controllers/microposts_controller.rb
+++ sample_app_rails_4/app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,6 +14,8 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
@@ -20,6 +23,11 @@ class MicropostsController < ApplicationController
     # リクエストをパースする
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 
 end
