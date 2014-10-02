cecc0a0155bf3814b9c1641f094f72c381c5a340
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 20:23:54 +0900

    10-39

--- sample_app_rails_4/app/controllers/microposts_controller.rb
+++ sample_app_rails_4/app/controllers/microposts_controller.rb
@@ -2,14 +2,12 @@ class MicropostsController < ApplicationController
   before_action :signed_in_user
 
   def create
-
-    # 新しいMicropostのオブジェクトを生成する
     @micropost = current_user.microposts.build(micropost_params)
-    
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
