fa243550208bfa6320f97459102e6aaa20ac2dd6
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 19:25:54 +0900

    10-36

--- sample_app_rails_4/app/models/user.rb
+++ sample_app_rails_4/app/models/user.rb
@@ -17,6 +17,12 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
