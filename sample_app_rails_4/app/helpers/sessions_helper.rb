dfd5f5f67124cc7608928d6f2761c3074958299f
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 18:39:50 +0900

    10-24

--- sample_app_rails_4/app/helpers/sessions_helper.rb
+++ sample_app_rails_4/app/helpers/sessions_helper.rb
@@ -23,6 +23,13 @@ module SessionsHelper
     @current_user = user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
