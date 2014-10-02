87543b45765f6aba12640ac0e809bbd6a1cd8d7e
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 16:11:18 +0900

    10-2

--- sample_app_rails_4/spec/models/micropost_spec.rb
+++ sample_app_rails_4/spec/models/micropost_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    # このコードは慣用的な意味で正しくない。
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
 end
