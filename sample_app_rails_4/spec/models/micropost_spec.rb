a28fe18bc3ea4395f03cd6a3c0f806249d9876db
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 16:26:09 +0900

    10-5

--- sample_app_rails_4/spec/models/micropost_spec.rb
+++ sample_app_rails_4/spec/models/micropost_spec.rb
@@ -2,19 +2,25 @@ require 'spec_helper'
 
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
-  before do
-    # このコードは慣用的な意味で正しくない。
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
-  end
 
+  # ユーザーに紐づくMicropostを自動生成する
+  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
   subject { @micropost }
 
+  # micropostモデルに必要な属性を保持しているか。
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
 
+  # micropostに関連するユーザーが取得できるか。
+  it { should respond_to(:user) }
+  its(:user) { should eq user }
+
+  # バリデーション
   it { should be_valid }
 
   describe "when user_id is not present" do
+
+    # ユーザーIDがnullは許可しない
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
