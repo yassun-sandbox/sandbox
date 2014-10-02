668bc737377048a90af0b28b2b644ac7c575b73a
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 18:33:02 +0900

    10-22

--- sample_app_rails_4/config/routes.rb
+++ sample_app_rails_4/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
