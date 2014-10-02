6b6bec7f81597ab9368ca9fd6e5d4e0ba70268aa
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-02 16:47:01 +0900

    10-10

--- sample_app_rails_4/spec/models/user_spec.rb
+++ sample_app_rails_4/spec/models/user_spec.rb
@@ -136,5 +136,22 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 
+  # 新しいポストが最初にくる事の確認
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+  end
+
 end
 
