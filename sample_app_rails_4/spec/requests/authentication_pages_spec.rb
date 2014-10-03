8f83bb169be4febfc1eb9567aeab6bb407219f5e
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 19:08:45 +0900

    11-33

--- sample_app_rails_4/spec/requests/authentication_pages_spec.rb
+++ sample_app_rails_4/spec/requests/authentication_pages_spec.rb
@@ -131,6 +131,20 @@ describe "Authentication" do
             end
 
           end
+
+          # サインインしてない状態でフォローアクションが呼ばれた場合の挙動
+          describe "in the Relationships controller" do
+            describe "submitting to the create action" do
+              before { post relationships_path }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+
+            describe "submitting to the destroy action" do
+              before { delete relationship_path(1) }
+              specify { expect(response).to redirect_to(signin_path) }
+            end
+          end
+
         end
 
         describe "as wrong user" do
