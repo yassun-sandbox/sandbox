903e34f221727fbdc8d119fa884dddc8be299fde
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 15:57:46 +0900

    11-6

--- sample_app_rails_4/app/models/relationship.rb
+++ sample_app_rails_4/app/models/relationship.rb
@@ -1,2 +1,5 @@
 class Relationship < ActiveRecord::Base
+  # foreign_keyを省略すると、Class名_idとなる。ここではuser_idとヒモ付ける
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 end
