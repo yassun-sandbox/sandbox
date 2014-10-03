ba6d383210a56142a4652f5954c4411272e30e91
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 19:40:59 +0900

    11-42

--- sample_app_rails_4/app/models/user.rb
+++ sample_app_rails_4/app/models/user.rb
@@ -30,9 +30,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    # このコードは準備段階です。
-    # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
