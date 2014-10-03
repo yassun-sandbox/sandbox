866530dc1052aa108c5ee053196d8139c979fcbc
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 19:48:17 +0900

    11-44

--- sample_app_rails_4/app/models/micropost.rb
+++ sample_app_rails_4/app/models/micropost.rb
@@ -4,9 +4,11 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
 
+
   def self.from_users_followed_by(user)
     followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+          followed_user_ids: followed_user_ids, user_id: user)
   end
 
 end
