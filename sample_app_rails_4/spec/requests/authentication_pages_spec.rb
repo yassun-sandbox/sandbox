1f4cda37a66b880f3c3ce00de92c930cbd658dde
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 18:36:42 +0900

    11-28

--- sample_app_rails_4/spec/requests/authentication_pages_spec.rb
+++ sample_app_rails_4/spec/requests/authentication_pages_spec.rb
@@ -66,54 +66,69 @@ describe "Authentication" do
         describe "for non-signed-in users" do
           let(:user) { FactoryGirl.create(:user) }
 
-          describe "when attempting to visit a protected page" do
-            before do
-              visit edit_user_path(user)
-              fill_in "Email",    with: user.email
-              fill_in "Password", with: user.password
-              click_button "Sign in"
-            end
-
-            describe "after signing in" do
-              it "should render the desired protected page" do
-                expect(page).to have_title('Edit user')
-              end
-            end
-          end
-
-          # ログインした際にMicropostにアクセスした場合
-          describe "in the Microposts controller" do
-
-            describe "submitting to the create action" do
-              before { post microposts_path }
-              specify { expect(response).to redirect_to(signin_path) }
-            end
-
-            describe "submitting to the destroy action" do
-              before { delete micropost_path(FactoryGirl.create(:micropost)) }
-              specify { expect(response).to redirect_to(signin_path) }
-            end
-          end
-
-          # ログインしていない場合
           describe "in the Users controller" do
 
-            # edit pageを訪れた場合
-            describe "visiting the edit page" do
-              before { visit edit_user_path(user) }
+            describe "when attempting to visit a protected page" do
+              before do
+                visit edit_user_path(user)
+                fill_in "Email",    with: user.email
+                fill_in "Password", with: user.password
+                click_button "Sign in"
+              end
+
+              describe "after signing in" do
+                it "should render the desired protected page" do
+                  expect(page).to have_title('Edit user')
+                end
+              end
+            end
+
+            # ログインした際にMicropostにアクセスした場合
+            describe "in the Microposts controller" do
+
+              describe "submitting to the create action" do
+                before { post microposts_path }
+                specify { expect(response).to redirect_to(signin_path) }
+              end
+
+              describe "submitting to the destroy action" do
+                before { delete micropost_path(FactoryGirl.create(:micropost)) }
+                specify { expect(response).to redirect_to(signin_path) }
+              end
+            end
+
+            # ログインしていない場合
+            describe "in the Users controller" do
+
+              # edit pageを訪れた場合
+              describe "visiting the edit page" do
+                before { visit edit_user_path(user) }
+                it { should have_title('Sign in') }
+              end
+
+              # Update要求を行った場合
+              describe "submitting to the update action" do
+                before { patch user_path(user) }
+                specify { expect(response).to redirect_to(signin_path) }
+              end
+
+              describe "visiting the user index" do
+                before { visit users_path }
+                it { should have_title('Sign in') }
+              end
+            end
+
+            # フォローユーザーとフォロワーユーザーでログインチェックされるか
+            describe "visiting the following page" do
+              before { visit following_user_path(user) }
               it { should have_title('Sign in') }
             end
 
-            # Update要求を行った場合
-            describe "submitting to the update action" do
-              before { patch user_path(user) }
-              specify { expect(response).to redirect_to(signin_path) }
-            end
-
-            describe "visiting the user index" do
-              before { visit users_path }
+            describe "visiting the followers page" do
+              before { visit followers_user_path(user) }
               it { should have_title('Sign in') }
             end
+
           end
         end
 
