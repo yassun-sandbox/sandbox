63f7e270c35623b1ca5f0eb3892d1c6dcc14a99c
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 19:40:44 +0900

    11-41

--- sample_app_rails_4/spec/models/user_spec.rb
+++ sample_app_rails_4/spec/models/user_spec.rb
@@ -177,10 +177,24 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+      before do
+         @user.follow!(followed_user)
+         3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
+      # フォローしてないユーザーのpostはfeedに表示されていない事の確認
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+
+      # フォローしているユーザーのpostはfeedに表示される
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+
     end
 
   end
