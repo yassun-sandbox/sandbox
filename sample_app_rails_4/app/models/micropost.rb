6f58eca55e92d5c688503ff6771cc53b2da9b1f3
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-03 19:45:59 +0900

    11-43

--- sample_app_rails_4/app/models/micropost.rb
+++ sample_app_rails_4/app/models/micropost.rb
@@ -3,5 +3,11 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
   default_scope -> { order('created_at DESC') }
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids
+    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+  end
+
 end
 
