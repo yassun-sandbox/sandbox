0a8f3aaf48edf8da315a28191ad43ea37fa3c500
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-09 14:32:45 +0900

    Add relationship between author and book

--- rails_study_book_admin/app/models/book.rb
+++ rails_study_book_admin/app/models/book.rb
@@ -1,5 +1,12 @@
 class Book < ActiveRecord::Base
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?","%#{theme}%") }
+
+  # Bookから見ればPublisherは一意に決まる
   belongs_to :publisher
+
+  # Bookから見てAuthorは多であり、BookAuthorsを挟んで参照する
+  has_many :book_authors
+  has_many :authers, through: :book_authors
+
 end
