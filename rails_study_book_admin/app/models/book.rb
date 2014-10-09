c18408226055ea19e2856e50e0abd9d801864ac4
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-09 14:39:45 +0900

    Add validates to bookmodel

--- rails_study_book_admin/app/models/book.rb
+++ rails_study_book_admin/app/models/book.rb
@@ -9,4 +9,10 @@ class Book < ActiveRecord::Base
   has_many :book_authors
   has_many :authers, through: :book_authors
 
+  # バリデーション
+  validates :name, presence: true
+  validates :name, length: { maximum:15 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
 end
+
