5d418113d016b43e187faf7a44a22be43af7d252
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-09 16:37:11 +0900

    add show method

--- rails_study_book_admin/app/controllers/books_controller.rb
+++ rails_study_book_admin/app/controllers/books_controller.rb
@@ -1,2 +1,10 @@
 class BooksController < ApplicationController
+  def show
+    @book = Book.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.csv
+      format.json
+    end
+  end
 end
