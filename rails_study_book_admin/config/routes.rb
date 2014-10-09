f6b277b4402336b0aebcc9d9ac4f8f1e92852e8f
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-09 16:43:20 +0900

    add publishers routeing

--- rails_study_book_admin/config/routes.rb
+++ rails_study_book_admin/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/books/:id" => "books#show"
+  resources :publishers
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
