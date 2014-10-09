bf7c51c2e6abbc68b81c40e7c64f66aa7867177a
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-09 14:14:17 +0900

    Add BookAuthor Model

--- rails_study_book_admin/db/schema.rb
+++ rails_study_book_admin/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009044550) do
+ActiveRecord::Schema.define(version: 20141009051317) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20141009044550) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "book_authors", force: true do |t|
+    t.integer  "book_id"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id"
+  add_index "book_authors", ["book_id"], name: "index_book_authors_on_book_id"
 
   create_table "books", force: true do |t|
     t.string   "name"
