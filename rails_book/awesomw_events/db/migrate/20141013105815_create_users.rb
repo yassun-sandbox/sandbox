d0f4da8fd85859359f89357bdee1940264f87bcf
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-13 20:01:37 +0900

    Add user model

---
+++ rails_book/awesomw_events/db/migrate/20141013105815_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :nickname
+      t.string :image_url
+
+      t.timestamps
+    end
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
