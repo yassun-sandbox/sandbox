f36f5a2551e643abc498d2b363544ad5719ddd2a
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-14 19:13:44 +0900

    Add dpendent destory ticket and event

--- rails_book/awesomw_events/app/models/event.rb
+++ rails_book/awesomw_events/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
-  has_many :tickets
+  has_many :tickets, dependent: :destory
 
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
