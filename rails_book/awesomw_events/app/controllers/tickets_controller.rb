fd9fd26ed58a7c1a323fbd8d357ca9acbe1926e4
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-14 19:11:39 +0900

    Add tickets cancel

--- rails_book/awesomw_events/app/controllers/tickets_controller.rb
+++ rails_book/awesomw_events/app/controllers/tickets_controller.rb
@@ -18,4 +18,10 @@ class TicketsController < ApplicationController
     end
   end
 
+  def destroy
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
+    ticket.destroy!
+    redirect_to event_path(params[:event_id]), notice: 'このイベントの参加をキャンセルしました'
+  end
+
 end
