d4962d8dcdd4357f01f91d3f6a53a4385110e9c8
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-15 16:12:32 +0900

    letの追加

--- rails_book/awesomw_events/spec/models/event_spec.rb
+++ rails_book/awesomw_events/spec/models/event_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe Event do
   describe '#name' do
     context '空白の時' do
+      let(:event){ Event.new(name:'') }
+
       it 'validでないこと' do
-        event = Event.new(name:'')
         event.valid?
         expect(event.errors[:name]).to be_present
       end
