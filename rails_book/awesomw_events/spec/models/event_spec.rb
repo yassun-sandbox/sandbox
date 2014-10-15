416f3594bf8791bf35a7e0af5eae4442840fabc4
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-15 16:26:43 +0900

    eventsモデルのバリデーションテストを追加

--- rails_book/awesomw_events/spec/models/event_spec.rb
+++ rails_book/awesomw_events/spec/models/event_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 
 describe Event do
   describe '#name' do
-    context '空白の時' do
+
+    context 'nilの場合' do
+      let(:event){ Event.new(name:nil) }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context '空白の場合' do
       let(:event){ Event.new(name:'') }
 
       it 'validでないこと' do
@@ -10,6 +20,34 @@ describe Event do
         expect(event.errors[:name]).to be_present
       end
     end
+
+    context '「Rails勉強会」の場合' do
+      let(:event){ Event.new(name:'Rails勉強会') }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context '50文字のとき' do
+      let(:event){ Event.new(name:'A' * 50) }
+
+      it 'validであること' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context '51文字のとき' do
+      let(:event){ Event.new(name:'A' * 51) }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
   end
 end
 
