4e680360975e5f0ed737adcc341a0dde7acee68a
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-29 18:48:59 +0900

    p46

--- everyday_rails_syakyo/spec/factories/contacts.rb
+++ everyday_rails_syakyo/spec/factories/contacts.rb
@@ -1,6 +1,7 @@
 require'faker'
 FactoryGirl.define do
   factory :contact do
+
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
     email { Faker::Internet.email }
@@ -10,5 +11,10 @@ FactoryGirl.define do
         contact.phones << FactoryGirl.build(:phone, phone_type: phone, contact: contact)
       end
     end
+
+    factory :invalid_contact do
+      firstname nil
+    end
+
   end
 end
