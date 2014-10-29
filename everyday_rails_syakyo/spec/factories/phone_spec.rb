e54f211bce3cae83d60e597474f9325500d67405
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-29 16:58:24 +0900

    p36 refactor phones factory

--- everyday_rails_syakyo/spec/factories/phone_spec.rb
+++ everyday_rails_syakyo/spec/factories/phone_spec.rb
@@ -1,8 +1,20 @@
 FactoryGirl.define do
+
   factory :phone do
+
     association :contact
     phone { '123-555-1234' }
-    phone_type 'home'
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+    factory :work_phone do
+      phone_type 'work'
+    end
+    factory :mobile_phone do
+      phone_type 'mobile'
+    end
   end
 end
+￼￼￼￼￼￼
 
--- everyday_rails_syakyo/spec/models/phone_spec.rb
+++ everyday_rails_syakyo/spec/models/phone_spec.rb
@@ -2,29 +2,18 @@ require'spec_helper'
 
 describe Phone do
 
-  # 連絡先ごとに重複した電話番号を許可しないこと
-  it "does not allow duplicate phone numbers per contact" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
-      email: 'joetester@example.com')
-    contact.phones.create(phone_type: 'home',
-      phone: '785-555-1234')
-    mobile_phone = contact.phones.build(phone_type: 'mobile',
-      phone: '785-555-1234')
+  it "連絡先ごとに重複した電話番号を許可しないこと" do
 
+    contact = create(:contact)
+    create(:home_phone, contact: contact, phone: '785-555-1234')
+    mobile_phone = build(:mobile_phone, contact: contact, phone: '785-555-1234')
     expect(mobile_phone).to have(1).errors_on(:phone)
+
   end
 
-  # 2 件の連絡先で同じ電話番号を共有できること
-  it "allows two contacts to share a phone number" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
-      email: 'joetester@example.com')
-    contact.phones.create(phone_type: 'home',
-      phone: '785-555-1234')
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(phone_type:
-      'home', phone: '785-555-1234')
-
-    expect(other_phone).to be_valid
+  it "2 件の連絡先で同じ電話番号を共有できること" do
+    create(:home_phone, phone: "785-555-1234")
+    expect(build(:home_phone, phone: "785-555-1234")).to be_valid
   end
 
 end
