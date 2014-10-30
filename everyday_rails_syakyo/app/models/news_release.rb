c9598bf5c8337ab7139606043d6d66c46acdb6ea
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-30 18:55:16 +0900

    p107

--- everyday_rails_syakyo/app/models/news_release.rb
+++ everyday_rails_syakyo/app/models/news_release.rb
@@ -1,2 +1,5 @@
 class NewsRelease < ActiveRecord::Base
+  def title_with_date
+    "#{released_on.strftime('%Y-%m-%d')}: #{title}"
+  end
 end
--- everyday_rails_syakyo/spec/models/news_release_spec.rb
+++ everyday_rails_syakyo/spec/models/news_release_spec.rb
@@ -1,5 +1,10 @@
-require 'spec_helper'
+require'spec_helper'
 
-describe NewsRelease do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe NewsRelease, focus:true do
+
+  it "フォーマットされた日付とタイトルの文字列を返すこと" do
+    news_release = NewsRelease.new( released_on: '2013-07-31', title: 'BigCo hires new CEO')
+    expect(news_release.title_with_date).to eq '2013-07-31: BigCo hires new CEO'
+  end
+
 end
