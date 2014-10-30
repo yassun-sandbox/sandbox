aca5b88c09d084854cc7be99e941bd6d817a6865
Taga Yasunori <yasukun.taga@gmail.com>
2014-10-30 19:01:37 +0900

    p108 add model spec

--- everyday_rails_syakyo/app/models/news_release.rb
+++ everyday_rails_syakyo/app/models/news_release.rb
@@ -1,4 +1,8 @@
 class NewsRelease < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released_on, presence: true
+  validates :body, presence: true
+
   def title_with_date
     "#{released_on.strftime('%Y-%m-%d')}: #{title}"
   end
