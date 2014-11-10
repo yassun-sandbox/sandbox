fab86b36e917169c183647bdb20d0c55e542477a
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-10 13:38:44 +0900

    webmockのBodyを外部ファイルから取得できるように修正

--- rails/front_app/spec/spec_helper.rb
+++ rails/front_app/spec/spec_helper.rb
@@ -86,3 +86,12 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(File.join(fixture_path, file))
+end
+
