f56607d70c8c1ac247d480fa1f17781dcd3076c0
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-10 15:53:42 +0900

    api_clientの試験を追加

--- rails/front_app/spec/lib/api_clients/json_api_client.rb
+++ rails/front_app/spec/lib/api_clients/json_api_client.rb
@@ -5,14 +5,22 @@ RSpec.describe JsonApiClient do
   describe '#get_json' do
 
     context 'APIからの戻り値が存在する場合' do
-      it "JSONパースされた結果が取得できること"
+      let(:user){ fixture_json('user1.json') }
+      before do
+        stub_request(:get, %r(#{JSON_API_URL}/users/1.*)).to_return({
+          :body   => fixture('user1.json'),
+          :status => 200
+        })
+      end
+
+      it "JSONパースされた結果が取得できること" do
+        expect(JsonApiClient.get_json(["users","1"])).to eq user
+      end
     end
 
     context 'APIからの戻り値が存在しない場合' do
       before do
-        stub_request(:get,"http://localhost:3333/").to_return({
-          :status => 404
-        })
+        stub_request(:get,"#{JSON_API_URL}").to_return({ :status => 404 })
       end
 
       it "nilが返却されること" do
@@ -21,6 +29,5 @@ RSpec.describe JsonApiClient do
     end
 
   end
-
 end
 
