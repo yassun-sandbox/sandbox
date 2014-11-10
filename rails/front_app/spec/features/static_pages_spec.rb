f179575bd974696484176912fc6d802857a600c6
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-10 16:04:29 +0900

    エンドツーエンドテスト追加

--- rails/front_app/spec/features/static_pages_spec.rb
+++ rails/front_app/spec/features/static_pages_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'StaticPages', :type => :feature do
   describe "indexを表示" do
 
     context 'APIからユーザー情報を取得できた場合' do
+      let(:user1){ fixture_json('user1.json') }
+      let(:user2){ fixture_json('user2.json') }
 
       before do
 
@@ -27,16 +29,30 @@ RSpec.describe 'StaticPages', :type => :feature do
       it 'ユーザー情報が表示されていること' do
         should have_title('FrontApp')
 
-        should have_selector('h1', text: 'hogehoge')
-        should have_selector('li', text: 1)
-        should have_selector('li', text: '111-1111-1111')
+        should have_selector('h1', text: user1["name"])
+        should have_selector('li', text: user1["id"])
+        should have_selector('li', text: user1["tel"])
 
-        should have_selector('h1', text: 'fugafuga')
-        should have_selector('li', text: 2)
-        should have_selector('li', text: '222-2222-2222')
+        should have_selector('h1', text: user2["name"])
+        should have_selector('li', text: user2["id"])
+        should have_selector('li', text: user2["tel"])
 
       end
     end
+
+    context 'APIからユーザー情報が取得できなかった場合' do
+      before do
+        stub_request(:get,"#{JSON_API_URL}").to_return({ :status => 404 })
+      end
+
+      it '何も表示されていない事' do
+        should_not have_selector('h1')
+        should_not have_selector('li')
+      end
+
+
+    end
+
   end
 end
 
