fdccd51ff887993bb113c0939840441ae519b56a
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-15 17:19:07 +0900

    アップデート処理追加

--- ruby/gem/todo/lib/todo/command.rb
+++ ruby/gem/todo/lib/todo/command.rb
@@ -11,12 +11,23 @@ module Todo
 
     def create_task(name, content)
       # タスク作成時のstatusはデフォルト値が使われNOT_YETとなる。
-      Task.create!(name: name, content: content, status: 0).reload
+      Task.create!(name: name, content: content).reload
     end
 
     def delete_task(id)
       task = Task.find(id)
       task.destroy
+    end
+
+    def update_task(id, attrbiutes)
+      if status_name = attributes[:status]
+        attributes[:status] = Task::STATUS.fetch(status_name.upcase)
+      end
+
+      task = Task.find(id)
+      task.update_attributes! attributes
+
+      task.reload
     end
 
   end
