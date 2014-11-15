eed192370fae4e5818dcb4aa96f454b0404d3f5d
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-15 16:28:40 +0900

    tasksテーブルの作成処理追加

--- ruby/gem/todo/lib/todo/db.rb
+++ ruby/gem/todo/lib/todo/db.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+require 'fileutils'
 require 'active_record'
 
 module Todo
@@ -10,9 +11,42 @@ module Todo
     # データベースへの接続とテーブルの作成を行う。
     # @return [void]
     def self.prepare
+      database_path = File.join(ENV['HOME'], '.todo', 'todo.sqlite3')
+
+      connect_database database_path
+      create_table_id_not_exeits database_path
+    end
+
+    def self.connect_database(path)
+      spec = { adapter: 'sqlite3', database: path }
+      ActiveRecord::Base.establish_connection spec
+    end
+
+    def self.create_table_id_not_exists(path)
+      create_database_path path
+
+      connection = ActiveRecord::Base.connection
+      return if connection.table_exists?(:tasks)
+
+      connection.create_table :tasks do |t|
+        t.column :name,    :string,  null:false
+        t.column :content, :text,    null:false
+        t.column :status,  :integer, null:false
+        t.timestamps
+      end
+
+      connection.add_index :tasks, :status
+      connection.add_index :tasks, :created_at
 
     end
 
+    def self.create_database_path(path)
+      FileUtils.mkdir_p File.dirname(path)
+    end
+
+    private_class_method :connect_database, :create_table_id_not_exists, :create_database_path
+
   end
+
 end
 
