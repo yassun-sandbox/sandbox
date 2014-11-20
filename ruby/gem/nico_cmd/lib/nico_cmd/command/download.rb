3f2776c4d02be237fcc50269f6878d919002be4d
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-20 10:29:38 +0900

    ログイン処理の実装変更

--- ruby/gem/nico_cmd/lib/nico_cmd/command/download.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/download.rb
@@ -7,13 +7,13 @@ module NicoCmd
     option :sm,  :type => :array,  :aliases => '-s',  :required => true, :desc => "sm number"
     option :mp3, :type => :boolean, :aliases => '-m', :required => true, :desc => "mp3 convert"
     def download
+      login unless @niconico
+
       ids = options[:sm]
 
-#      # ログイン
-#      niconico = get_niconico
 #
 #      # 動画情報の取得
-#      video = niconico.video(id)
+#      video = @niconico.video(id)
 #      puts video.id
 #      puts video.title
 #      puts video.description
@@ -29,12 +29,11 @@ module NicoCmd
 
     private
 
-    def get_niconico
+    def login
       yaml_path = File.expand_path(File.dirname(__FILE__)+"../../../../user.yml")
       user = YAML.load_file(yaml_path)
-      niconico = Niconico.new(user["mail"], user["pass"])
-      niconico.login
-      niconico
+      @niconico = Niconico.new(user["mail"], user["pass"])
+      @niconico.login
     end
 
   end
