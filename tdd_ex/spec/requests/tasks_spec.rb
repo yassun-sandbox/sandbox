079b88d0695dfa3f53e86a920eac4f2e4e105420
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-22 15:47:33 +0900

    createメソッドの追加

--- tdd_ex/spec/requests/tasks_spec.rb
+++ tdd_ex/spec/requests/tasks_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe "Tasks", :type => :feature do
   describe "タスクの追加について" do
     it "登録すると一覧に表示される事" do
       visit "tasks/new"
-      fill_in "Tile", with: "おしごと１"
+      fill_in "Title", with: "おしごと１"
       fill_in "Contents", with: "しょうさい"
       click_button "追加"
 
