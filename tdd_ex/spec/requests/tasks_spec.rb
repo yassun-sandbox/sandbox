de545bb8f11ec92d3e9cc382866d874dd6710bc3
Taga Yasunori <yasukun.taga@gmail.com>
2014-11-22 14:48:32 +0900

    タスク追加画面のテスト追加

--- tdd_ex/spec/requests/tasks_spec.rb
+++ tdd_ex/spec/requests/tasks_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "Tasks", :type => :feature do
   describe "GET /tasks/new" do
     it "タスク追加画面が表示できる事" do
       visit "tasks/new"
-      expect(response).to have_http_status(200)
+      expect(page).to have_http_status(200)
+      expect(page).to have_content('タスク追加')
     end
   end
 
