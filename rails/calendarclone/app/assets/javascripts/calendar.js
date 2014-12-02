169ca594bf1d6ba67a63ba73b82ee808af6c269b
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-02 11:42:50 +0900

    selectable option追加

--- rails/calendarclone/app/assets/javascripts/calendar.js
+++ rails/calendarclone/app/assets/javascripts/calendar.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
     $('#calendar').fullCalendar({
-    })
+        events: '/events.json',
+        selectable: true,
+        selectHelper: true,
+        select: function(start, end, allDay) {
+            console.log('start:' + start);
+            console.log('end:' + end);
+            console.log('allDay:' + allDay);
+            alert('selected');
+        }
+    });
 });
