b52956d5d1dfa6baf65959cb945c3bc36dc8154e
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-02 11:59:36 +0900

    add post ajax

--- rails/calendarclone/app/assets/javascripts/calendar.js
+++ rails/calendarclone/app/assets/javascripts/calendar.js
@@ -1,13 +1,28 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
+
+    var select = function(start, end, allDay) {
+        var title = window.prompt("title");
+        var data = { event: {title: title,
+                            start: start,
+                            end: end,
+                            allDay: allDay}};
+        $.ajax({
+            type: "POST",
+            url: "/events",
+            data: data,
+            success: function() {
+                calendar.fullCalendar('refetchEvents');
+            }
+        });
+        calendar.fullCalendar('unselect');
+    };
+
+    var calendar = $('#calendar').fullCalendar({
         events: '/events.json',
         selectable: true,
         selectHelper: true,
-        select: function(start, end, allDay) {
-            console.log('start:' + start);
-            console.log('end:' + end);
-            console.log('allDay:' + allDay);
-            alert('selected');
-        }
+        ignoreTimezone: false,
+        select: select
     });
+
 });
