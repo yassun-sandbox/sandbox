8d9318ca935ef72e2dfd9c7f92b7fda7073ef8f8
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-06 19:02:21 +0900

    add login_info

--- ruby/gem/shukkin/lib/shukkin/command.rb
+++ ruby/gem/shukkin/lib/shukkin/command.rb
@@ -6,8 +6,16 @@ module Shukkin
   class Command < Thor
     desc 'shukkin', 'start'
     def shukkin
-      puts "shukkin"
+      login_info = get_login_info
     end
+
+    private
+
+    def get_login_info
+      yaml_path = File.expand_path(File.dirname(__FILE__)+"../../../../tuba.yml")
+      YAML.load_file(yaml_path)
+    end
+
   end
 end
 
