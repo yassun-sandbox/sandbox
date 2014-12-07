74149ea5f795f86151b3a6599736713e5253e2e7
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-07 17:42:52 +0900

    ログイン実施

--- ruby/gem/shukkin/lib/shukkin/command.rb
+++ ruby/gem/shukkin/lib/shukkin/command.rb
@@ -6,10 +6,18 @@ module Shukkin
   class Command < Thor
     desc 'shukkin', 'start'
     def shukkin
+
+      # ログイン情報の取得
       login_info = get_login_info
 
       agent = Mechanize.new
       agent.get(login_info["url"]) do |page|
+
+        # ログイン
+        response = page.form_with(:action => '/auth/remotelogin') do |form|
+          form.field_with(:name => 'post[login]').value    = login_info["id"]
+          form.field_with(:name => 'post[password]').value = login_info["pass"]
+        end.click_button
       end
 
     end
