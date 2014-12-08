11f785295d4af606d1a4a9df1e16a88e1945edca
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-08 11:10:41 +0900

    設定ファイル名称変更

--- ruby/gem/shukkin/lib/shukkin/command.rb
+++ ruby/gem/shukkin/lib/shukkin/command.rb
@@ -7,28 +7,28 @@ module Shukkin
     desc 'shukkin', 'start'
     def shukkin
 
-      # ログイン情報の取得
-      login_info = get_login_info
+      # サイト情報の取得
+      site_info = get_site_info
 
       agent = Mechanize.new
-      agent.get(login_info["url"]) do |page|
+      agent.get(site_info["url"]) do |page|
 
         # ログイン
         page.form_with(:action => '/auth/remotelogin') do |form|
-          form.field_with(:name => 'post[login]').value    = login_info["id"]
-          form.field_with(:name => 'post[password]').value = login_info["pass"]
+          form.field_with(:name => 'post[login]').value    = site_info["id"]
+          form.field_with(:name => 'post[password]').value = site_info["pass"]
         end.click_button
 
       end
 
-      p agent.post(login_info["punch_in_url"],{},login_info["header"]);
+      p agent.post(site_info["punch_in_url"], {}, site_info["header"]);
 
     end
 
     private
 
-    def get_login_info
-      yaml_path = File.expand_path(File.dirname(__FILE__)+"../../../tuba.yml")
+    def get_site_info
+      yaml_path = File.expand_path(File.dirname(__FILE__)+"../../../site_info.yml")
       YAML.load_file(yaml_path)
     end
 
