f744cee170abb64a90682c215efaf63fb67460ba
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-09 11:03:57 +0900

    CSRFとCookieをリクエストヘッダに追加

--- ruby/gem/shukkin/lib/shukkin/command.rb
+++ ruby/gem/shukkin/lib/shukkin/command.rb
@@ -14,14 +14,28 @@ module Shukkin
       agent.get(site_info["url"]) do |page|
 
         # ログイン
-        page.form_with(:action => '/auth/remotelogin') do |form|
+        response = page.form_with(:action => '/auth/remotelogin') do |form|
           form.field_with(:name => 'post[login]').value    = site_info["id"]
           form.field_with(:name => 'post[password]').value = site_info["pass"]
         end.click_button
 
+        # CSRFトークン
+        csrf_token = response.at('meta[@name="csrf-token"]')[:content]
+        csrf_hash  = { 'X-CSRF-Token' => csrf_token }
+
+        # Cookie
+        cookie_hash = get_cookie_hash(site_info["cookie"], agent.cookie_jar.jar)
+
+        # 共通ヘッダー
+        header = site_info["header"]
+
+        # マージ
+        header.merge!(csrf_hash).merge!(cookie_hash)
+
+        agent.post(site_info["punch_in_url"], {}, header);
+
       end
 
-      p agent.post(site_info["punch_in_url"], {}, site_info["header"]);
 
     end
 
@@ -30,6 +44,17 @@ module Shukkin
     def get_site_info
       yaml_path = File.expand_path(File.dirname(__FILE__)+"../../../site_info.yml")
       YAML.load_file(yaml_path)
+    end
+
+    def get_cookie_hash(cookie_info, cookie_jar)
+        domain = cookie_info["domain"]
+        path   = cookie_info["path"]
+        name   = cookie_info["name"]
+
+        cookie_name = cookie_jar[domain][path][name].name
+        cookie_val  = cookie_jar[domain][path][name].value
+        cookie = cookie_name + "=" + cookie_val
+        { "Cookie" => cookie }
     end
 
   end
