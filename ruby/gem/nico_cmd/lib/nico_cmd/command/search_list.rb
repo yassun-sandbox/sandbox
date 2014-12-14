4c9459a244b198a530f2533cf5debe425a438b81
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-14 13:54:43 +0900

    add url param

--- ruby/gem/nico_cmd/lib/nico_cmd/command/search_list.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/search_list.rb
@@ -4,7 +4,10 @@ module NicoCmd
   class Command < Thor
 
     desc 'search_list', 'download search video list '
+    option :url, :type => :string,  :aliases => '-u', :required => true, :desc => "target url"
     def search_list
+      agent = Mechanize.new
+      page = agent.get(options[:url])
     end
 
   end
