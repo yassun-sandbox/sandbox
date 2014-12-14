b9f9663be7ab25665584770c7e1318a6c8f40a38
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-14 13:38:39 +0900

    add search_list

--- ruby/gem/nico_cmd/lib/nico_cmd/command.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command.rb
@@ -4,6 +4,7 @@ require 'yaml'
 require 'open3'
 require "niconico"
 require "nico_cmd/command/download"
+require "nico_cmd/command/search_list"
 
 module NicoCmd
   class Command < Thor
---
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/search_list.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+module NicoCmd
+
+  class Command < Thor
+
+    desc 'search_list', 'download search video list '
+    def search_list
+    end
+
+  end
+end
+
