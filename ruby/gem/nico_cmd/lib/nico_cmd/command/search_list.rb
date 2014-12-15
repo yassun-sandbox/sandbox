db46a8e3af3488ba6cf15f591427fca26913ac78
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-15 09:49:46 +0900

    video linkの取得

--- ruby/gem/nico_cmd/lib/nico_cmd/command/search_list.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/search_list.rb
@@ -8,6 +8,17 @@ module NicoCmd
     def search_list
       agent = Mechanize.new
       page = agent.get(options[:url])
+      page.links.each do | link |
+
+        if %r(/watch/sm[\d]?) =~ link.href
+          puts " -------------- "
+          puts link.text
+          puts nico_id = link.href.scan(/sm[\d]+/)[0]
+          puts "http://nicovideo.jp/#{nico_id}"
+          puts " -------------- "
+        end
+
+      end
     end
 
   end
