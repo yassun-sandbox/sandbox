4e9977dfa5007c3cb8b946e1cfb398aecbdee711
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-16 10:01:20 +0900

    mylist idを引数から取得

--- ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
@@ -4,7 +4,11 @@ module NicoCmd
   class Command < Thor
 
     desc 'my_list', 'mylist list videos '
-    def my_list
+    option :mylist_id, :type => :string, :aliases => '-m',
+      :required => true, :desc => "target mylist id"
+    def mylist
+      login unless @niconico
+      p @niconico.mylist(options[:mylist_id])
     end
   end
 end
