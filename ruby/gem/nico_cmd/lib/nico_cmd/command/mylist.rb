c3e3f06a7d737e1b8ec041fcd164077e265cdd66
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-17 09:34:01 +0900

    add mylist dl

--- ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
@@ -4,11 +4,28 @@ module NicoCmd
   class Command < Thor
 
     desc 'my_list', 'mylist list videos '
-    option :mylist_id, :type => :string, :aliases => '-m',
-      :required => true, :desc => "target mylist id"
+    option :mylist_id, :type => :string, :aliases => '-m', :required => true, :desc => "target mylist id"
+    option :dir, :type => :string,  :aliases => '-d', :required => true, :desc => "target dir"
+
     def mylist
       login unless @niconico
-      p @niconico.mylist(options[:mylist_id])
+
+      target_dir = get_target_dir(options[:dir])
+
+      @niconico.mylist(options[:mylist_id]).each do | video |
+
+        puts video
+
+        # ファイルの保存
+        flv_path = dl_flv(target_dir, video)
+
+        # 説明文の保存
+        dl_ext_text(target_dir, video)
+
+        # MP3への変換
+        convert_mp3(flv_path)
+      end
+
     end
   end
 end
