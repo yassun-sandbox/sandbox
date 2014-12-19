9828823e8bd6fd1a61553b6c4e6dc1a24eba458b
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-19 10:13:18 +0900

    beginning_of_weekとend_of_weekの追加

--- rails/calendar_sample/app/controllers/calendar_controller.rb
+++ rails/calendar_sample/app/controllers/calendar_controller.rb
@@ -1,4 +1,7 @@
 class CalendarController < ApplicationController
   def index
+    from_date = Date.new(2014, 11, 1).beginning_of_week(:sunday)
+    to_date = Date.new(2014, 11, 30).end_of_week(:sunday)
+    @calendar_data = from_date.upto(to_date)
   end
 end
