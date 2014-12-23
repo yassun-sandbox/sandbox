0c6dc00ff86b48613c6a13432b3e4e36ac8407de
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-23 20:23:45 +0900

    add ruboty-fizzbuzz

---
+++ ruby/gem/ruboty-fizzbuzz/lib/ruboty/fizzbuzz.rb
@@ -0,0 +1,7 @@
+require "ruboty/fizzbuzz/version"
+
+module Ruboty
+  module Fizzbuzz
+    # Your code goes here...
+  end
+end
---
+++ ruby/gem/ruboty-fizzbuzz/lib/ruboty/fizzbuzz/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Fizzbuzz
+    VERSION = "0.0.1"
+  end
+end
