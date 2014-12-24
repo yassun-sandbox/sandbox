02429bf2787365bfdf39e2466e1c7953c627546b
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-24 09:35:07 +0900

    add Fizz Buzz

--- ruby/gem/ruboty-fizzbuzz/lib/ruboty/fizzbuzz/actions/fizzbuzz.rb
+++ ruby/gem/ruboty-fizzbuzz/lib/ruboty/fizzbuzz/actions/fizzbuzz.rb
@@ -5,6 +5,21 @@ module Ruboty
         def call
           message.reply('dummy')
         end
+
+        private
+
+        def fizzbuzz
+          case message[:number].to_i
+          when fizzbuzz? then "FizzBuzz"
+          when buzz? then "Buzz"
+          when fizz? then "Fizz"
+          else message[:number]
+          end
+        end
+
+        def fizzbuzz?; ->(v){v % 15 == 0}; end
+        def buzz?; ->(v){v % 5 == 0}; end
+        def fizz?; ->(v){v % 3 == 0}; end
       end
     end
   end
