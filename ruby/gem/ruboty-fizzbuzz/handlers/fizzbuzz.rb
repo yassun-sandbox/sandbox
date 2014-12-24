6f91af548e762a9af12386a774e2f160eec1d650
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-24 09:28:50 +0900

    add action call

--- ruby/gem/ruboty-fizzbuzz/handlers/fizzbuzz.rb
+++ ruby/gem/ruboty-fizzbuzz/handlers/fizzbuzz.rb
@@ -3,7 +3,11 @@ require "ruboty/fizzbuzz/actions/fizzbuzz"
 module Ruboty
   module Handlers
     class Fizzbuzz < Base
-      on /fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output fizzbuzz result'
+      on %r/fizzbuzz (?<number>.*?)\z/, name: 'fizzbuzz', description: 'output fizzbuzz result'
+
+      def fizzbuzz(message)
+        Ruboty::Fizzbuzz::Actions::Fizzbuzz.new(message).call
+      end
     end
   end
 end
