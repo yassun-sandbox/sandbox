be7ad8b0b1f886b09c43e0720b348a3f2522b5a8
Taga Yasunori <yasukun.taga@gmail.com>
2014-12-31 03:21:43 +0900

    add tw callback

--- rails/devise_sample/app/models/user.rb
+++ rails/devise_sample/app/models/user.rb
@@ -4,4 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:twitter]
+
+  def self.from_omniauth(auth)
+    # providerとuidでUserレコードを取得する
+    # 存在しない場合は、ブロック内のコードを実行して作成する
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      # auth.provider には "twitter"、
+      # auth.uidには twitterアカウントに基づいた個別のIDが入っている
+      # first_or_createメソッドが自動でproviderとuidを設定してくれるので、
+      # ここでは設定は必要ない
+      user.username = auth.info.nickname # twitterで利用している名前が入る
+      user.email = auth.info.email # twitterの場合入らない
+    end
+  end
+
 end
