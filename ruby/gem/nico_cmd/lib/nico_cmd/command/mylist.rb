ae65c3198d346afb212fa44655dead40f9bf685a
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-09 10:48:28 +0900

    improved mylist

--- ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
+++ ruby/gem/nico_cmd/lib/nico_cmd/command/mylist.rb
@@ -3,7 +3,7 @@ module NicoCmd
 
   class Command < Thor
 
-    desc 'my_list', 'mylist list videos '
+    desc 'mylist', 'mylist list videos '
     option :mylist_id, :type => :string, :aliases => '-m', :required => true, :desc => "target mylist id"
     option :dir, :type => :string,  :aliases => '-d', :required => true, :desc => "target dir"
 
@@ -13,14 +13,11 @@ module NicoCmd
       target_dir = get_target_dir(options[:dir])
 
       @niconico.mylist(options[:mylist_id]).each do | video |
-
-        puts video
+        file_name = video.title.gsub(" ", "")
+        puts file_name
 
         # ファイルの保存
-        flv_path = dl_flv(target_dir, video)
-
-        # 説明文の保存
-        dl_ext_text(target_dir, video)
+        flv_path = dl_flv(target_dir, file_name ,video)
 
         # MP3への変換
         convert_mp3(flv_path)
