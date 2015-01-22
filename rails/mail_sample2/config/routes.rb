a76283455cef3ff91921cf1e3d55bfdf3c70e242
Taga Yasunori <yasukun.taga@gmail.com>
2015-01-22 10:17:31 +0900

    add routes

--- rails/mail_sample2/config/routes.rb
+++ rails/mail_sample2/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'inquiry'          => 'inquiry#index'     # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
