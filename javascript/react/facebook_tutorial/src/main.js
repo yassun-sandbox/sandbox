b10fbccf39c1e184df0810dab5abe9702f9fb5a2
Taga Yasunori <yasukun.taga@gmail.com>
2015-02-20 09:28:26 +0900

    tutorial5

--- javascript/react/facebook_tutorial/src/main.js
+++ javascript/react/facebook_tutorial/src/main.js
@@ -1,14 +1,51 @@
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
 
 React.render(
-    <CommentBox />,
-    document.getElementById('content')
+  <CommentBox />,
+  document.getElementById('content')
 );
+
