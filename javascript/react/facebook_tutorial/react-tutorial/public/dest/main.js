85199dc088ef267a8f293f5d80897b2608a83151
Taga Yasunori <yasukun.taga@gmail.com>
2015-02-23 09:35:36 +0900

    tutorial16

--- javascript/react/facebook_tutorial/react-tutorial/public/dest/main.js
+++ javascript/react/facebook_tutorial/react-tutorial/public/dest/main.js
@@ -32,11 +32,22 @@ var CommentList = React.createClass({displayName: "CommentList",
 });
 
 var CommentForm = React.createClass({displayName: "CommentForm",
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: send request to the server
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+  },
   render: function() {
     return (
-      React.createElement("form", {className: "commentForm"}, 
-        React.createElement("input", {type: "text", placeholder: "Your name"}), 
-        React.createElement("input", {type: "text", placeholder: "Say something..."}), 
+     React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
+        React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something...", ref: "text"}), 
         React.createElement("input", {type: "submit", value: "Post"})
       )
     );
--- javascript/react/facebook_tutorial/react-tutorial/public/src/main.js
+++ javascript/react/facebook_tutorial/react-tutorial/public/src/main.js
@@ -32,11 +32,22 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: send request to the server
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+  },
   render: function() {
     return (
-      <form className="commentForm">
-        <input type="text" placeholder="Your name" />
-        <input type="text" placeholder="Say something..." />
+     <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
         <input type="submit" value="Post" />
       </form>
     );
