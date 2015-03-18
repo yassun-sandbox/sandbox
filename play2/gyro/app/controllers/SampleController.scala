fbf34d21c505d7099688e5b5ebf52dc0a37a0e5f
Taga Yasunori <yasukun.taga@gmail.com>
2015-03-18 11:21:57 +0900

    add bind param

--- play2/gyro/app/controllers/SampleController.scala
+++ play2/gyro/app/controllers/SampleController.scala
@@ -15,4 +15,15 @@ object SampleController extends Controller{
   def sample2 = Action {
     Ok(views.html.index("Sample Controller#sample2"))
   }
+
+  def sample5(fixedValue: String) = Action {
+    println("fixedValue:" + fixedValue)
+    Ok(views.html.index("fixedValue:" + fixedValue))
+  }
+
+  def sample6(defaultValue:Int) = Action {
+    println("defaultValue:" + defaultValue)
+    Ok(views.html.index("defaultValue:"+ defaultValue))
+  }
+
 }
