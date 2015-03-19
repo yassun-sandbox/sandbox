30659a1222e1722004127b0c274183b62bc13190
Taga Yasunori <yasukun.taga@gmail.com>
2015-03-19 10:53:53 +0900

    add type parameter function

--- scala/type_parameter/src/MySample.scala
+++ scala/type_parameter/src/MySample.scala
@@ -5,4 +5,8 @@ class MySample [A]{
   var param:A = _
   def get:A = param
   def set(param:A) = this.param = param
+
+  // Functionに型パラメータを付与
+  def func[A](arg:A):A = arg
+
 }
--- scala/type_parameter/src/typeParameter.scala
+++ scala/type_parameter/src/typeParameter.scala
@@ -5,8 +5,18 @@ object typeParameter {
 
   def main (args: Array[String]): Unit = {
     val x = new MySample[String]
-    x.set("hello")
+    x.set("hello1")
     println(x.get)
+
+    // 型パラメータ有りファンクション
+
+    // 型推論
+    println(x.func("hello2"))
+
+    // 型指定
+    println(x.func[String]("hello3"))
+    println(x.func[Int](4))
+
   }
 
 }
