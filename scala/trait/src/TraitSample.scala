bfd618ca6679a66ccc08741ab9b67a4003a98a76
Taga Yasunori <yasukun.taga@gmail.com>
2015-03-23 14:25:06 +0900

    add trait constructor pattern

--- scala/trait/src/TraitSample.scala
+++ scala/trait/src/TraitSample.scala
@@ -5,6 +5,12 @@ object TraitSample {
   def main(args: Array[String]) {
     val p = new Person("taro")
     p.coding
+
+    // traitのコンストラクタを実行
+    val p2 = new Person("taro") with ProjectManager
+    p2.manage
+
+
   }
 
 }
@@ -14,3 +20,11 @@ trait Programmer {
 }
 
 class Person(val name:String) extends Programmer
+
+// コンストラクタ付きトレイト
+trait ProjectManager {
+  val budget:Int = 100000000
+  println("budget="+ budget)
+
+  def manage = println("プロジェクト管理します")
+}
