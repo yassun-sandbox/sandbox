fe9277bc6b127944bf101f1cc16b8b08ecd60967
Taga Yasunori <yasukun.taga@gmail.com>
2015-03-31 09:44:21 +0900

    add routing

--- play2/play2-hands-on/app/controllers/Application.scala
+++ play2/play2-hands-on/app/controllers/Application.scala
@@ -3,10 +3,34 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+object UserController extends Controller {
+
+  /**
+   * 一覧表示
+   */
+  def list = TODO
+
+  /**
+   * 登録・編集画面表示
+   */
+  def edit(id: Option[Long]) = TODO
+
+  /**
+   * 登録実行
+   */
+  def create = TODO
+
+  /**
+   * 更新実行
+   */
+  def update = TODO
+
+  /**
+   * 削除実行
+   */
+  def remove(id: Long) = TODO
 
 }
+
+
