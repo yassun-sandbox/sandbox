a964c228836f7d02df7c66b0f66ebb11ef3bfc3d
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-01 11:36:06 +0900

    add HelloWorld Swift

---
+++ swift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by YasunoriTaga on 2015/05/01.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("Hello World")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
