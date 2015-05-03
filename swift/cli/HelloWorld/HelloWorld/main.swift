0d3f5e56ef8a21ea103acc4bed78445d77faa802
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-03 14:46:17 +0900

    add swift cli HelloWorld

---
+++ swift/cli/HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by YasunoriTaga on 2015/05/03.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
