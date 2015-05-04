3e3517a876ab086332959d5239907a67295ffbe9
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-04 14:35:35 +0900

    add YenFromDoll sample

---
+++ swift/cli/YenFromDoll/YenFromDoll/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  YenFromDoll
+//
+//  Created by YasunoriTaga on 2015/05/04.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
+
+let rate = 108.8
+var doll = 10.5
+
+
+var yen = doll * rate
+
+println("\(doll)ドルは\(yen)円です")
+
