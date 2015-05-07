d6e1a171087194dcd5d6da7971bfd9aee58971ec
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-07 13:19:25 +0900

    add Optional Sample

---
+++ swift/cli/SwiftStudy/SwiftStudy/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  SwiftStudy
+//
+//  Created by YasunoriTaga on 2015/05/07.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
+
+// Optional
+
+// int型のオプショナル
+var num:Int? = 5
+
+// アンラップ
+var sum = num! + 5
+
