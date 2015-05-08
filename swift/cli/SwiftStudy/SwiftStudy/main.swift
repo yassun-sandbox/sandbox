e6faf459aa7dfa0cc89ac7c218c79d040517b521
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-08 14:22:51 +0900

    外部引数

--- swift/cli/SwiftStudy/SwiftStudy/main.swift
+++ swift/cli/SwiftStudy/SwiftStudy/main.swift
@@ -22,3 +22,12 @@ var names = ["hoge","huga","hega"]
 
 // 辞書
 var customers = [10: "HOGE",11: "HUGA",12:"HEGA"]
+
+// 外部引数
+func stdWeight(heightCm height: Double, bmiValue bmi: Double) -> Double{
+    var heightM = height / 100.0
+    return heightM * heightM * bmi
+}
+
+let height = 180.0
+let w1 = stdWeight(heightCm: height, bmiValue: 22.0)
