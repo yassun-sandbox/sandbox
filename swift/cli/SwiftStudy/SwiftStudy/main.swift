49d2f19734b42bc9986f720478086414f4181896
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-08 14:39:09 +0900

    タプルを戻す関数

--- swift/cli/SwiftStudy/SwiftStudy/main.swift
+++ swift/cli/SwiftStudy/SwiftStudy/main.swift
@@ -32,3 +32,11 @@ func stdWeight(heightCm height: Double, bmi: Double = 22.0) -> Double{
 let height = 180.0
 let w1 = stdWeight(heightCm: height, bmi: 22.0)
 
+// タプルを返却する関数
+func maxMin(v1:Double, v2:Double, v3:Double) -> (Double, Double) {
+    var maxValue = max(v1, v2, v3)
+    var minValue = min(v1, v2, v3)
+    return (maxValue, minValue)
+}
+var (max1, min1) = maxMin(9.5, 3.0, 10.0)
+println("max: \(max1), min:\(min1)")
