956a9a021651d3e9afc5f58bfa0a0e1891701f55
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-10 14:36:33 +0900

    引数のデフォルトはlet

--- swift/cli/Playground/FunctionSample.playground/section-1.swift
+++ swift/cli/Playground/FunctionSample.playground/section-1.swift
@@ -17,3 +17,11 @@ func multiplyAll(nums: Double...) -> Double {
 let m0 = multiplyAll()
 let m1 = multiplyAll(4.0, 5.0, 1.5)
 let m2 = multiplyAll(4.0, 5.0, 1.5, 2.0)
+
+// 引数はデフォルトではlet
+// 明示的にverをつける必要がある
+func cmToInch(var cm: Double) -> Double {
+    cm = cm * 0.3937
+    return cm
+}
+
