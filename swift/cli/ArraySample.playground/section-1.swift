32ad5105d97c4b1cc5ca1489f759a4ce242b1df9
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-13 10:33:09 +0900

    add reduce sample

--- swift/cli/ArraySample.playground/section-1.swift
+++ swift/cli/ArraySample.playground/section-1.swift
@@ -22,3 +22,17 @@ let htmlFiles = files.filter({$0.hasSuffix(".html")})
 let inchArray = [2.0, 3.5, 1.0, 5.3]
 let cmArray   = inchArray.map{$0 * 2.54}
 
+// reduce
+var sum1 = numbers.reduce(0){$0 + $1}
+var sum2 = numbers.reduce(0,+)
+
+var colors = ["red","green","blue","black"]
+var colorsStr = colors.reduce(""){
+    if $0 != "" {
+        return $0 + "," + $1
+    } else {
+        return $1
+    }
+}
+println(colorsStr)
+
