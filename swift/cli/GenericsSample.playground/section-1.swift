09ee7cf9847f5bac831fa1a308891e29aa9e22c4
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-14 09:49:27 +0900

    add Generics Class Sample

--- swift/cli/GenericsSample.playground/section-1.swift
+++ swift/cli/GenericsSample.playground/section-1.swift
@@ -13,3 +13,19 @@ func largerValue<T: Comparable>(a: T, b: T) -> T {
     return a > b ? a : b
 }
 largerValue(100,200)
+
+// クラス定義
+class Product<T> {
+    var serialNumber:T
+    var name:String
+    init(serialNumber:T, name:String){
+        self.serialNumber = serialNumber
+        self.name = name
+    }
+}
+
+var p1 = Product<String>(serialNumber: "A001", name: "hoge")
+var p2 = Product<String>(serialNumber: "A002", name: "huga")
+
+var p3 = Product<Int>(serialNumber: 100, name: "p3")
+var p4 = Product<Int>(serialNumber: 200, name: "p4")
