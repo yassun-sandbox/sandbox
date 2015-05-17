2a7106ed602521a64498ea5e8cd2131070b1d140
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-17 14:04:23 +0900

    add Propety Sample

---
+++ swift/cli/RobotSample/RobotSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RobotSample
+//
+//  Created by YasunoriTaga on 2015/05/17.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
