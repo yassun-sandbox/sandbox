dc09a61d1735d46370447ae1f05aead3649595e7
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-18 09:57:11 +0900

    add TyeProperySample

---
+++ swift/cli/TypePropety.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+// クラスタイプ・プロパティ
+class myCalc {
+    class var PI:Double {
+        return 3.14
+    }
+    class var DoublePi: Double {
+        return self.PI * 2
+    }
+}
+
