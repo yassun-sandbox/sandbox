8158ec25613a8948c593999c3850b459dc6dc202
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-19 09:27:58 +0900

    add Calender Sample

---
+++ swift/cli/CalenderTest1/CalenderTest1/MyCalender.swift
@@ -0,0 +1,9 @@
+//
+//  MyCalender.swift
+//  CalenderTest1
+//
+//  Created by YasunoriTaga on 2015/05/19.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
---
+++ swift/cli/CalenderTest1/CalenderTest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalenderTest1
+//
+//  Created by YasunoriTaga on 2015/05/19.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
