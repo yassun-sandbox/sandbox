de9c40528864a61d9bc6bd0c352ed3d8b6c9a891
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-21 10:02:13 +0900

    Add Hello iPhone Project

---
+++ swift/HelloiPhone/HelloiPhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloiPhone
+//
+//  Created by YasunoriTaga on 2015/05/21.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
