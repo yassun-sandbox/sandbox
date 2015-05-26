60d8bed3b1161548ab6c4468c0d8047740c629a2
Taga Yasunori <yasukun.taga@gmail.com>
2015-05-26 09:46:41 +0900

    add AssistantEditor

--- swift/xcode6Test/xcode6Test/ViewController.swift
+++ swift/xcode6Test/xcode6Test/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func myButton(sender: AnyObject) {
+       myLabel.text = "hogehoge"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
