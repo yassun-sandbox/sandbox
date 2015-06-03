7d8dfc18a769c91f8586a11796b102417d68b068
Taga Yasunori <yasukun.taga@gmail.com>
2015-06-03 10:49:10 +0900

    Add TabbedApp Sample

---
+++ swift/TabbedAppSample/TabbedAppSample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedAppSample
+//
+//  Created by YasunoriTaga on 2015/06/03.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ swift/TabbedAppSample/TabbedAppSampleTests/TabbedAppSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  TabbedAppSampleTests.swift
+//  TabbedAppSampleTests
+//
+//  Created by YasunoriTaga on 2015/06/03.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TabbedAppSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
