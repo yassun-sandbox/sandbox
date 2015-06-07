3b1e2623ba512465e23d188bd71ddcd26d2d0117
Taga Yasunori <yasukun.taga@gmail.com>
2015-06-07 15:31:07 +0900

    Add TabEventSample

---
+++ swift/TabEventSample/TabEventSample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabEventSample
+//
+//  Created by YasunoriTaga on 2015/06/07.
+//  Copyright (c) 2015年 Yasuun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
