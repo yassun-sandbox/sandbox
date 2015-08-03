bbd634dc51b944f1eaaabd9bb55e7a8fce0ac6c5
Taga Yasunori <yasukun.taga@gmail.com>
2015-08-03 12:20:40 +0900

    Add todo route

--- volt/todo_example/app/main/config/routes.rb
+++ volt/todo_example/app/main/config/routes.rb
@@ -9,6 +9,8 @@ client '/password_reset', component: 'user_templates', controller: 'password_res
 client '/forgot', component: 'user_templates', controller: 'login', action: 'forgot'
 client '/account', component: 'user_templates', controller: 'account', action: 'index'
 
+client '/todos', action: 'todos'
+
 # The main route, this should be last. It will match any params not
 # previously matched.
 client '/', {}
