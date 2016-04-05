983990155e740d7544f733e461ce6b8d280f531b
Taga Yasunori <yasukun.taga@gmail.com>
2016-04-05 19:10:14 +0800

    add show message

--- rails/rails5/campfire/app/channels/room_channel.rb
+++ rails/rails5/campfire/app/channels/room_channel.rb
@@ -9,7 +9,7 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    ActionCable.server.broadcast 'room_channel', message: data['message']
+    Message.create! content: data['message']
   end
 
 end
