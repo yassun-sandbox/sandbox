79fc1161d3480405b3dc1e25c8bcc2dbe686c599
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-12 00:04:36 +0800

    add new app.module.ts

--- javascript/angular2/angular-tour-of-heroes/app/app.module.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [
@@ -11,9 +14,14 @@ import { HeroDetailComponent } from './hero-detail.component';
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
 
--- javascript/angular2/angular-tour-of-heroes/app/heroes.component.ts
+++ javascript/angular2/angular-tour-of-heroes/app/heroes.component.ts
@@ -4,7 +4,6 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   template: `
-    <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
@@ -64,10 +63,8 @@ import { HeroService } from './hero.service';
       border-radius: 4px 0 0 4px;
     }
   `],
-  providers: [HeroService]
 })
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
   constructor(private heroService: HeroService) { }
