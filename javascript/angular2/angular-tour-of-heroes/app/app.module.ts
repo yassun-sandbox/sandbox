4f978467e848754fa6cb64fc84b685353a821201
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-13 23:52:37 +0800

    add view template

--- javascript/angular2/angular-tour-of-heroes/app/app.module.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.module.ts
@@ -20,6 +20,10 @@ import { DashboardComponent }  from './dashboard.component';
         pathMatch: 'full'
       },
       {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
       },
