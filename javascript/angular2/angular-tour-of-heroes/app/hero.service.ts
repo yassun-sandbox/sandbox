0c92c20fa15eae6431cab15dffb3ee2d755655f2
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-19 10:26:38 +0800

    fix from promise to Rx

--- javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
+++ javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
@@ -1,14 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes';
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
   }
+
   getHero(id: number): Promise<Hero> {
     return this.getHeroes()
                .then(heroes => heroes.find(hero => hero.id === id));
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
+
 
