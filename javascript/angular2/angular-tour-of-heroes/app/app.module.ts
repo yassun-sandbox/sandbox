ed39413c799d05428e27f47ac859e71ac5f131fd
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-20 11:09:24 +0800

    add InMemory for mock server

--- javascript/angular2/angular-tour-of-heroes/app/app.module.ts
+++ javascript/angular2/angular-tour-of-heroes/app/app.module.ts
@@ -5,6 +5,10 @@ import { FormsModule }    from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { HttpModule }     from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { DashboardComponent }   from './dashboard.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HeroesComponent }      from './heroes.component';
@@ -15,6 +19,7 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
