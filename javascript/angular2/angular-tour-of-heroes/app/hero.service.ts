10e028d1373918e20ee59e04905e85ac21e5ffcd
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-25 12:47:52 +0900

    add create method

--- javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
+++ javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
@@ -30,6 +30,13 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
