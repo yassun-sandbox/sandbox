81a7cecc9e909c64084e95558aa7d2b615f885ae
Taga Yasunori <yasukun.taga@gmail.com>
2016-11-27 19:35:41 +0800

    add delet emethod

--- javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
+++ javascript/angular2/angular-tour-of-heroes/app/hero.service.ts
@@ -37,6 +37,13 @@ export class HeroService {
       .then(res => res.json().data)
       .catch(this.handleError);
   }
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
