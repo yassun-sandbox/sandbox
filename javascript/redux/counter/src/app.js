7a925635a43b052d0785af9f60bfe8694809d93f
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-02 12:58:23 +0700

    add test

--- javascript/redux/counter/src/app.js
+++ javascript/redux/counter/src/app.js
@@ -1,3 +1,19 @@
+import expect from 'expect'
+
 const counter = (state, action) => {
   return state
 }
+
+expect(
+  counter(0, {type: 'INCREMENT'})
+).toEqual(1)
+
+expect(
+  counter(1, {type: 'INCREMENT'})
+).toEqual(2)
+
+expect(
+  counter(2, {type: 'DECREMENT'})
+).toEqual(1)
+
+console.log('test passed!')
