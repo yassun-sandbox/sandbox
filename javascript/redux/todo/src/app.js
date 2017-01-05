a4c9156ace1471d075b2393f2dc024cd225ab32c
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-05 19:16:40 +0800

    Writing a Todo List Reducer (Adding a Todo)

---
+++ javascript/redux/todo/src/app.js
@@ -0,0 +1,49 @@
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
+import { createStore } from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ];
+      default:
+        return state;
+  }
+};
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+
+};
+
+testAddTodo();
+console.log("All tests passed");
+
