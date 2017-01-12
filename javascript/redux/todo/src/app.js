41961c5bbe41e11c507fd495a219735fd44ac8a8
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-12 18:38:43 +0800

    Passing the Store Down Implicitly via Context

--- javascript/redux/todo/src/app.js
+++ javascript/redux/todo/src/app.js
@@ -79,7 +79,7 @@ const getVisibleTodos = (
 
 class VisibleTodoList extends Component {
   componentDidMount() {
-    const { store } = this.props;
+    const { store } = this.context;
     this.unsubscribe = store.subscribe(() =>
       this.forceUpdate()
     );
@@ -91,7 +91,7 @@ class VisibleTodoList extends Component {
 
   render() {
     const props = this.props;
-    const { store } = props;
+    const { store } = this.context;
     const state = store.getState();
 
     return (
@@ -112,13 +112,16 @@ class VisibleTodoList extends Component {
     );
   }
 }
+VisibleTodoList.contextTypes = {
+  store: React.PropTypes.object
+};
 
 let nextTodoId = 0;
 const TodoApp = ({ store }) => (
   <div>
-    <AddTodo store={store} />
-    <VisibleTodoList store={store} />
-    <Footer store={store} />
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
   </div>
 );
 
@@ -156,7 +159,7 @@ const Todo = ({
 </li>
 );
 
-const AddTodo = ({ store }) => {
+const AddTodo = (props, { store }) => {
   let input;
 
   return (
@@ -176,6 +179,9 @@ const AddTodo = ({ store }) => {
       </button>
     </div>
   );
+};
+AddTodo.contextTypes = {
+  store: React.PropTypes.object
 };
 
 const Link = ({
@@ -200,7 +206,7 @@ const Link = ({
 
 class FilterLink extends Component {
   componentDidMount() {
-    const { store } = this.props;
+    const { store } = this.context;
     this.unsubscribe = store.subscribe(() =>
       this.forceUpdate()
     );
@@ -212,9 +218,8 @@ class FilterLink extends Component {
 
   render() {
     const props = this.props;
-    const { store } = props;
+    const { store } = this.context;
     const state = store.getState();
-    console.log(state);
 
     return (
       <Link
@@ -234,28 +239,28 @@ class FilterLink extends Component {
     );
   }
 };
+FilterLink.contextTypes = {
+  store: React.PropTypes.object
+};
 
-const Footer = ({ store }) => (
+const Footer = () => (
     <p>
       Show:
       {' '}
       <FilterLink
         filter='SHOW_ALL'
-        store={store}
       >
         ALL
       </FilterLink>
       {' '}
       <FilterLink
         filter='SHOW_ACTIVE'
-        store={store}
       >
         Active
       </FilterLink>
       {' '}
       <FilterLink
         filter='SHOW_COMPLETED'
-        store={store}
       >
         Completed
       </FilterLink>
@@ -263,8 +268,26 @@ const Footer = ({ store }) => (
 
 );
 
+class Provider extends Component {
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
+Provider.childContextTypes = {
+  store: React.PropTypes.object
+};
+
 ReactDOM.render(
-  <TodoApp store = {createStore(todoApp)} />,
+  <Provider store={createStore(todoApp)}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('root')
 );
 
