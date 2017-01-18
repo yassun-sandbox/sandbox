6471640aaaab8b6bf63bce3765ada169b1b2bc54
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-18 21:54:07 +0900

    connect dom and component

--- javascript/redux-saga/sample_counter/src/entry.js
+++ javascript/redux-saga/sample_counter/src/entry.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./containers/app";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
