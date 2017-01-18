1096711e52e39d2588929b3145723eb012ffa23c
Taga Yasunori <yasukun.taga@gmail.com>
2017-01-18 22:20:17 +0900

    add containers

---
+++ javascript/redux-saga/sample_counter/src/containers/app.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { increment, decrement, incrementIfOdd, incrementAsync } from "../actions";
+
+class App extends Component {
+  handleIncrement() {
+    this.props.dispatch(increment());
+  }
+
+  handleDecrement() {
+    this.props.dispatch(decrement());
+  }
+
+  handleIncrementIfOdd() {
+    this.props.dispatch(incrementIfOdd());
+  }
+
+  handleIncrementAsync() {
+    this.props.dispatch(incrementAsync());
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter example</h1>
+        <p>
+          Clicked: { this.props.counter } times
+          {" "}
+          <button className="increment" onClick={ ::this.handleIncrement }>+</button>
+          {" "}
+          <button className="decrement" onClick={ ::this.handleDecrement }>-</button>
+          {" "}
+          <button className="incrementIfOdd" onClick={ ::this.handleIncrementIfOdd }>Increment if odd</button>
+          {" "}
+          <button className="incrementAsync" onClick={ ::this.handleIncrementAsync }>Increment async</button>
+        </p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    counter: state.counter
+  };
+}
+
+export default connect(mapStateToProps)(App);
