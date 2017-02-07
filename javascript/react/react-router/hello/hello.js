955d4b1f2d2ec75fc5e109ce6eab2b8b499c29fe
Taga Yasunori <yasukun.taga@gmail.com>
2017-02-07 23:28:10 +0900

    add routing

--- javascript/react/react-router/hello/hello.js
+++ javascript/react/react-router/hello/hello.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { IndexRoute, Link, Router, Route, hashHistory } from 'react-router';
 
 class App extends Component {
     render() {
         return (
             <div>
-                Hello, react-router!
+                <ul>
+                    <li><Link to="/">index</Link></li>
+                    <li><Link to="/a">page A</Link></li>
+                    <li><Link to="/b">page B</Link></li>
+                </ul>
+                <div>
+                    { this.props.children }
+                </div>
             </div>
         );
     }
 }
 
+class Index extends Component {
+    render() {
+        return <div>Index</div>;
+    }
+}
+
+class PageA extends Component {
+    render() {
+        return <div>A</div>;
+    }
+}
+
+class PageB extends Component {
+    render() {
+        return <div>B</div>;
+    }
+}
+
 render((
     <Router history={hashHistory}>
-        <Route path="/" component={App} />
+        <Route path="/" component={App}>
+            <IndexRoute component={Index} />
+            <Route path="/a" component={PageA} />
+            <Route path="/b" component={PageB} />
+        </Route>
     </Router>
 ), document.querySelector('#app'));
+
