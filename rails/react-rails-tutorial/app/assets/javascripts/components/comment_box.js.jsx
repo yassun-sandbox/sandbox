10412d9ee23c734a085426135ec64bd6fc9a72fe
Taga Yasunori <yasukun.taga@gmail.com>
2017-03-08 17:29:37 +0900

    add api call

--- rails/react-rails-tutorial/app/assets/javascripts/components/comment_box.js.jsx
+++ rails/react-rails-tutorial/app/assets/javascripts/components/comment_box.js.jsx
@@ -1,9 +1,28 @@
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(result) {
+        this.setState({data: result.data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -11,9 +30,16 @@ var CommentBox = React.createClass({
 });
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        {commentNodes}
       </div>
     );
   }
@@ -27,3 +53,16 @@ var CommentForm = React.createClass({
     );
   }
 });
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
--- rails/react-rails-tutorial/config/routes.rb
+++ rails/react-rails-tutorial/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root 'comments#index'
+
+
+
+  namespace :api, format: 'json' do
+    namespace :v1  do
+      resources :comments
+    end
+  end
 end
