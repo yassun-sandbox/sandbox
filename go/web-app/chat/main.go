1da6acbf9a3b973e558a360fbf4e7e37e138d88f
Taga Yasunori <yasukun.taga@gmail.com>
2018-09-25 17:46:10 +0900

    add chat struct

--- go/web-app/chat/main.go
+++ go/web-app/chat/main.go
@@ -26,8 +26,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
+	// チャットルームを開始します
+	go r.run()
+
+	// Webサーバーを起動します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
--- go/web-app/chat/room.go
+++ go/web-app/chat/room.go
@@ -1,6 +1,94 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
+	// joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	// leaveはチャットルームから退室しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	// clientsには在室しているすべてのクライアントが保持されます。
+	clients map[*client]bool
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			// チャンネルのクローズ
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					// チャンネルのクローズ
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+// HTTPハンドラ用のメソッド
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	// Webソケットを使うための処理
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+
+	// チャットルームに入る(run側で拾われるイベント)
+	r.join <- client
+
+	// このスコープから抜けるなら退出する。
+	defer func() { r.leave <- client }()
+
+	// 別のスレッドで動作する。
+	go client.write()
+
+	// メインスレッドで無限ループ
+	client.read()
 }
