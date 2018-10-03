291ba91715a0bef017c758da3ca4fe27bef98cc2
Taga Yasunori <yasukun.taga@gmail.com>
2018-10-03 23:25:49 +0900

    add env

--- go/web-app/chat/main.go
+++ go/web-app/chat/main.go
@@ -9,8 +9,21 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+
 	"./trace"
 )
+
+func Env_load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 // テンプレートハンドラ
 type templateHandler struct {
@@ -30,11 +43,25 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	Env_load()
+
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
+
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("12345")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("CLIENT_ID"), os.Getenv("SECRET_KEY"), "http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	// 状態を持たない場合は関数を使うでもよい
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// チャットルームを開始します
