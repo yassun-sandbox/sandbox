6dd5520dbdf5a5ddcc03c62a8b33619ec66d6857
Taga Yasunori <yasukun.taga@gmail.com>
2020-08-16 01:13:34 +0900

    add env

--- go/slack/sample1.go
+++ go/slack/sample1.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/slack-go/slack"
 )
 
+func Env_load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
-	api := slack.New("YOUR_TOKEN_HERE")
+	api := slack.New(os.Getenv("SAMPLE_TOKEN"), slack.OptionDebug(true))
 	// If you set debugging, it will log all requests to the console
 	// Useful when encountering issues
 	// slack.New("YOUR_TOKEN_HERE", slack.OptionDebug(true))
