f73c34eae5abf27b1a1d28e48b1c107aca927ff1
yassun <yassun910@gmail.com>
2020-09-27 18:17:42 +0900

    add tagname

--- pragmatic-terraform-on-aws/example/main.tf
+++ pragmatic-terraform-on-aws/example/main.tf
@@ -1,4 +1,12 @@
 resource "aws_instance" "example" {
     ami = "ami-0c3fd0f5d33134a76"
     instance_type = "t3.micro"
+    tags = {
+        Name = "example"
+    }
+    user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
