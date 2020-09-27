9f0ae76799976858ba218c9c7ad5828f159bd499
yassun <yassun910@gmail.com>
2020-09-27 19:38:01 +0900

    4

--- pragmatic-terraform-on-aws/example/main.tf
+++ pragmatic-terraform-on-aws/example/main.tf
@@ -1,12 +1,8 @@
-resource "aws_instance" "example" {
-    ami = "ami-0c3fd0f5d33134a76"
-    instance_type = "t3.micro"
-    tags = {
-        Name = "example"
-    }
-    user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+module "dev_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
+}
+
+output "public_dns" {
+  value = module.dev_server.public_dns
 }
