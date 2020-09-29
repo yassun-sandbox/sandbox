ee6dc855f44b3a4a4b5806ca489f34c78e0fe8db
yassun <yassun910@gmail.com>
2020-09-30 00:57:01 +0900

    add alb

--- pragmatic-terraform-on-aws/example/7/alb.tf
+++ pragmatic-terraform-on-aws/example/7/alb.tf
@@ -42,3 +42,39 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+
+resource "aws_lb_target_group" "example" {
+  name                 = "example"
+  vpc_id               = aws_vpc.example.id
+  target_type          = "ip"
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = 200
+    port                = "traffic-port"
+    protocol            = "HTTP"
+  }
+
+  depends_on = [aws_lb.example]
+}
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/*"]
+  }
+}
