a0433647f78c15024362dedd48c4fc2621eb2a60
yassun <yassun910@gmail.com>
2021-04-16 16:26:50 +0900

    use clap

--- rust/introduction-to-rust-programming/samplecli/src/main.rs
+++ rust/introduction-to-rust-programming/samplecli/src/main.rs
@@ -1,6 +1,31 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("Your name")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::new("formula_file")
+                .about("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .about("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
