fea01bbcdae3df73f76351dfe612b0cdc89c2461
yassun <yassun910@gmail.com>
2021-04-17 15:09:09 +0900

    add Calculator

--- rust/introduction-to-rust-programming/samplecli/src/main.rs
+++ rust/introduction-to-rust-programming/samplecli/src/main.rs
@@ -2,6 +2,59 @@ use clap::Clap;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    pub fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            // 数値の場合
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            // 演算子の場合
+            } else {
+                // かならず2つとれる。
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                // 戻す
+                stack.push(res);
+            }
+
+            // 無名フィールドの参照
+            // RpnCalculator(bool)のbool
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        // 演算結果
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
 // アトリビュート
 #[derive(Clap, Debug)]
 #[clap(
@@ -36,8 +89,11 @@ fn main() {
 
 // トレイト境界: R = BufReadを実装している型
 fn run<R: BufRead>(reader: R, verbose: bool){
-    for line in reader.lines(){
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
