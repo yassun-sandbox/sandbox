0bf054c2d5a7e78f8d2a1fd69858b5e76ef428b4
yassun <yassun910@gmail.com>
2021-04-19 15:22:09 +0900

    add html template

--- rust/introduction-to-rust-programming/todo/src/main.rs
+++ rust/introduction-to-rust-programming/todo/src/main.rs
@@ -1,14 +1,48 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
 use thiserror::Error;
+use askama::Template;
+
+// Entity
+struct TodoEntry{
+    id: u32,
+    text: String,
+}
+
+// テンプレートとのマッピング
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate{
+    entries: Vec<TodoEntry>,
+}
 
 #[derive(Error,Debug)]
-enum MyError{}
+enum MyError{
+    // askamaエラーもMyErrorに含める
+    #[error("Faild to render HTML")]
+    AskamaError(#[from] askama::Error)
+}
 impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries  = Vec::new();
+    entries.push(TodoEntry{
+        id: 1,
+        text: "First".to_string() //Strは固定長なので変換が必要
+    });
+    entries.push(TodoEntry{
+        id: 2,
+        text: "Second".to_string()
+    });
+
+    // 変数をbind
+    let html = IndexTemplate{entries};
+    let response_body = html.render()?;
+    Ok(
+        HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body)
+    )
 }
 
 #[actix_rt::main]
