7cc54136b0281dc91cccd75b489021c90a887bd4
yassun <yassun910@gmail.com>
2021-06-19 13:17:21 +0900

    A Starting Point

--- rust/bubble-shell/src/main.rs
+++ rust/bubble-shell/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin};
+use std::process::{Command};
+
+fn main(){
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    // read_line leaves a trailing newline, which trim removes
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
