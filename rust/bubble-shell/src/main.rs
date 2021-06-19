5960b004c2232d8640bb2f238682478d1aa4e692
yassun <yassun910@gmail.com>
2021-06-19 22:57:49 +0900

    Handling Args

--- rust/bubble-shell/src/main.rs
+++ rust/bubble-shell/src/main.rs
@@ -11,9 +11,14 @@ fn main(){
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let command = input.trim();
+        // everything after the first whitespace character
+        //     is interpreted as args to the command
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
         let mut child = Command::new(command)
+            .args(args)
             .spawn()
             .unwrap();
 
