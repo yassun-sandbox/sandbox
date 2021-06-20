013e54d0c39071c89dad48131285a94173b0c6d9
yassun <yassun910@gmail.com>
2021-06-20 15:30:06 +0900

    Pipes

--- rust/bubble-shell/src/main.rs
+++ rust/bubble-shell/src/main.rs
@@ -1,46 +1,76 @@
 use std::env;
-use std::io::{stdin, stdout, Write};
-use std::process::{Command};
+use std::io::{stdin, stdout, Write };
+use std::process::{Child, Command, Stdio};
 use std::path::Path;
 
 fn main(){
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
         print!("> ");
-        stdout().flush().unwrap();
+        stdout().flush();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        // everything after the first whitespace character
-        //     is interpreted as args to the command
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        // must be peekable so we know when we are on the last command
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => {
-                // default to '/' as new directory if one was not provided
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                // ディレクトリの移動
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next()  {
+
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek()
+                        .map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => {
+                    let stdin = previous_command
+                        .map_or(
+                            Stdio::inherit(),
+                            |output: Child| Stdio::from(output.stdout.unwrap())
+                        );
+
+                    let stdout = if commands.peek().is_some() {
+                        // there is another command piped behind this one
+                        // prepare to send output to the next command
+                        Stdio::piped()
+                    } else {
+                        // there are no more commands piped behind this one
+                        // send output to shell stdout
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => { previous_command = Some(output); },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        },
+                    };
                 }
-            },
-            "exit" => return,
-            command => {
-                let child = Command::new(command)
-                    .args(args)
-                    .spawn();
-
-                // gracefully handle malformed user input
-                match child {
-                    Ok(mut child) => { child.wait().unwrap(); },
-                    Err(e) => eprintln!("{}", e),
-                };
             }
         }
+
+        if let Some(mut final_command) = previous_command {
+            // block until the final command has finished
+            final_command.wait();
+        }
+
     }
 }
