6bfa9c5292fd5113556ecf98d8728de82fa7754c
yassun <yassun910@gmail.com>
2021-06-21 18:16:13 +0900

    Write a function that adds tasks

--- rust/rusty-journal/src/tasks.rs
+++ rust/rusty-journal/src/tasks.rs
@@ -1,8 +1,12 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
@@ -12,3 +16,31 @@ impl Task {
         Task { text, created_at }
     }
 }
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    // Consume the file's contents as a vector of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Rewind the file after reading from it.
+    file.seek(SeekFrom::Start(0))?;
+
+    // Write the modified task list back into the file.
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+//pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> { ... }
+//pub fn list_tasks(journal_path: PathBuf) -> Result<()> { ... }
