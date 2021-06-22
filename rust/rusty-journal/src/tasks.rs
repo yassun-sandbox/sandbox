7d39c7ac0abddd8da2e416cfa41fd728ab56bc14
yassun <yassun910@gmail.com>
2021-06-22 22:11:19 +0900

    Refactor the Task collection

--- rust/rusty-journal/src/tasks.rs
+++ rust/rusty-journal/src/tasks.rs
@@ -18,27 +18,14 @@ impl Task {
 }
 
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
-    // Open the file.
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(journal_path)?;
-
-    // Consume the file's contents as a vector of tasks.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
-
-    // Rewind the file after reading from it.
-    file.seek(SeekFrom::Start(0))?;
-
-    // Write the modified task list back into the file.
+    let mut tasks = collect_tasks(&file)?;
     tasks.push(task);
     serde_json::to_writer(file, &tasks)?;
-
     Ok(())
 }
 
@@ -70,5 +57,17 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
     serde_json::to_writer(file, &tasks)?;
     Ok(())
 }
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?; // Rewind the file before.
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?; // Rewind the file after.
+    Ok(tasks)
+}
+
 
 //pub fn list_tasks(journal_path: PathBuf) -> Result<()> { ... }
