90f377212e6508ae17d5c2c8a2e6d5bd9b70e1fe
yassun <yassun910@gmail.com>
2021-08-15 13:48:01 +0900

    async_std::task ver

--- rust/async-book/async-1/src/main.rs
+++ rust/async-book/async-1/src/main.rs
@@ -1,10 +1,25 @@
-use futures::executor::block_on;
-
-async fn hello_world() {
-    println!("hello, world!");
-}
+use async_std::task;
 
 fn main() {
-    let future = hello_world();
-    block_on(future);
+    let mut futures = vec![]; // ベクター初期化
+
+    for i in 0..10 {
+        // クロージャを定義
+        let async_function = async move {
+            println!("{}", i);
+        };
+
+        // いま定義したクロージャを実行する非同期タスクを生成 + スケジューリングしている
+        let handle = task::spawn(async_function);
+
+        // 非同期タスクのハンドラーを待機リストに入れる
+        futures.push(handle);
+    }
+
+    task::block_on(async {
+        for f in futures {
+          // 各々の非同期タスクの完了を待つ
+            f.await
+        }
+    });
 }
