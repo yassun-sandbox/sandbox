73e7f4dddf19a40fa3162925cad1d3d2085e5795
yassun <yassun910@gmail.com>
2021-08-28 11:10:27 +0900

    hello

--- rust/introduction-to-rust-programming/chapter5/todo/src/main.rs
+++ rust/introduction-to-rust-programming/chapter5/todo/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, MyError> {
+    let response_body = "Hello world!";
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), actix_web::Error> {
+    HttpServer::new(move || App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
