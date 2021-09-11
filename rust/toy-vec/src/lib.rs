e5440222c83c60b8c47ff4b2bf53b61a36cf2089
yassun <yassun910@gmail.com>
2021-09-11 13:56:29 +0900

    79-2

--- rust/toy-vec/src/lib.rs
+++ rust/toy-vec/src/lib.rs
@@ -2,3 +2,27 @@ pub struct ToyVec<T> {
     elements: Box<[T]>,
     len: usize,
 }
+
+// Default: https://www.utam0k.jp/blog/2018/05/28/rust_std_default/
+impl<T: Default> ToyVec<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: 0,
+        }
+    }
+
+    // Vecを作成してBoxに変換
+    fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+}
+
