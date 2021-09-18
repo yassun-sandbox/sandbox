a44837eb2261f719729f3f30e7e736e585e282ca
yassun <yassun910@gmail.com>
2021-09-18 17:57:18 +0900

    1-3

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -9,5 +9,10 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
