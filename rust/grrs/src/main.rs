50d1959423150d10a87dc4848a07fd33b2db6fab
yassun <yassun910@gmail.com>
2021-09-18 17:53:29 +0900

    1-2

---
+++ rust/grrs/src/main.rs
@@ -0,0 +1,13 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+}
